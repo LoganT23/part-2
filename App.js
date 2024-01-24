e887ce1098e2e0e92efd51d276296fba706d682b
LoganT23 <Logan_Thomsen@hotmail.com>
2024-01-23 19:10:18 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+function LastThree () {
+    return (
+     <div>
+        <Tweet
+        username="LukasT"
+        date="3/10"
+        message="What the hockey puck?"
+        name="Logan" />
+
+<Tweet
+        username="Lucky777"
+        date="7/07"
+        message="It's my lucky day"
+        name="Miles" />
+
+<Tweet
+        username="TartyTart"
+        date="9/24"
+        message="To be, or not to be. That is the real question."
+        name="Ally" />
+
+</div>
+    );
+}
